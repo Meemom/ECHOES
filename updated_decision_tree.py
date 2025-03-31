07dd688f1df2986986744d2a0b6a82a73f03f670
Meemom <166151461+Meemom@users.noreply.github.com>
2025-03-31 12:37:57 -0400

    Update updated_decision_tree.py

--- updated_decision_tree.py
+++ updated_decision_tree.py
@@ -2,10 +2,12 @@
 
 This module contains functions for fitting a decision tree to a music dataset,
 enabling song recommendations based on a randomly selected song.
+
+We have marked each place you to modify code with the word "TODO" in order to run the file.
 """
 
 # Standard Library imports
-import os
+from __future__ import annotations
 import random
 from collections import Counter
 from typing import Any, Optional
@@ -35,11 +37,13 @@ class Node:
     """
     feature: Optional[int]
     threshold: Optional[float]
-    left: Optional['Node']
-    right: Optional['Node']
+    left: Optional[Node]
+    right: Optional[Node]
     value: Optional[Any]
 
-    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None) -> None:
+    def __init__(self, feature: Optional[int] = None, threshold: Optional[float] = None,
+                 left: Optional[Node] = None, right: Optional[Node] = None,
+                 value: Optional[int] = None) -> None:
         """Initialize a Node class."""
         self.feature = feature
         self.threshold = threshold
@@ -67,55 +71,56 @@ class DecisionTree:
     n_features: Optional[int]
     root: Optional[Node]
 
-    def __init__(self, min_samples_split=10, max_depth=5, n_features=None) -> None:
+    def __init__(self, min_samples_split: int = 2, max_depth: int = 5, n_features: Optional[int] = None) -> None:
         """Initializes a DecisionTree class."""
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.n_features = n_features
         self.root = None
 
-    def fit(self, X, y) -> None:
+    def fit(self, x_data: np.ndarray, y_data: np.ndarray) -> None:
         """Fits a decision tree to the dataset."""
         # check that self.n_features is not more than the actual number of features
-        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
-        self.root = self._grow_tree(X, y)  # builds a decision tree based on the training data
+        self.n_features = x_data.shape[1] if not self.n_features else min(x_data.shape[1], self.n_features)
+        self.root = self._grow_tree(x_data, y_data)  # builds a decision tree based on the training data
 
-    def _grow_tree(self, X, y, depth=0) -> Node:
+    def _grow_tree(self, x_data: np.ndarray, y_data: np.ndarray, depth: int = 0) -> Node:
         """Recursively grows the decision tree."""
-        n_samples, n_feats = X.shape  # get the number of samples and the number of features in the current node
-        n_labels = len(np.unique(y))  # get the number of unique labels in the target variable
+        n_samples, n_feats = x_data.shape  # get the number of samples and the number of features in the current node
+        n_labels = len(np.unique(y_data))  # get the number of unique labels in the target variable
 
         # check the stopping criteria
         if depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split:
             # stop growing and return the most common label at this node
-            leaf_value = self._most_common_label(y)
+            leaf_value = self._most_common_label(y_data)
             return Node(value=leaf_value)
 
         # only select unique features
         feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
 
         # find the best split
-        best_feature, best_threshold = self._best_split(X, y, feat_idxs)
+        best_feature, best_threshold = self._best_split(x_data, y_data, feat_idxs)
 
         # create child nodes
-        left_idxs, right_idxs = self._split(X[:, best_feature], best_threshold)
-        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
-        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        left_idxs, right_idxs = self._split(x_data[:, best_feature], best_threshold)
+        left = self._grow_tree(x_data[left_idxs, :], y_data[left_idxs], depth + 1)
+        right = self._grow_tree(x_data[right_idxs, :], y_data[right_idxs], depth + 1)
 
         return Node(feature=best_feature, threshold=best_threshold, left=left, right=right)
 
-    def _best_split(self, X, y, feat_idxs) -> tuple[Optional[int], Optional[float]]:
+    def _best_split(self, x_data: np.ndarray, y_data: np.ndarray,
+                    feat_idxs: list[int]) -> tuple[Optional[int], Optional[float]]:
         """Find the best threshold among all possible thresholds."""
         best_gain = -1
         split_idx, split_threshold = None, None
 
         for feat_idx in feat_idxs:
-            X_column = X[:, feat_idx]
-            thresholds = np.unique(X_column)
+            x_column = x_data[:, feat_idx]
+            thresholds = np.unique(x_column)
 
             for threshold in thresholds:
                 # calculate the information gain
-                gain = self._information_gain(y, X_column, threshold)
+                gain = self._information_gain(y_data, x_column, threshold)
 
                 if gain > best_gain:
                     best_gain = gain
@@ -124,7 +129,7 @@ class DecisionTree:
 
         return split_idx, split_threshold
 
-    def _information_gain(self, y, X_column, threshold) -> float:
+    def _information_gain(self, y_data: np.ndarray, x_column: np.ndarray, threshold: float) -> float:
         """Compute the information gain from splitting a dataset at a given threshold.
 
         Preconditions:
@@ -132,60 +137,60 @@ class DecisionTree:
             - X.column.size == y.size
         """
         # parent entropy
-        parent_entropy = self._entropy(y)
+        parent_entropy = self._entropy(y_data)
 
         # create children
-        left_idxs, right_idxs = self._split(X_column, threshold)
+        left_idxs, right_idxs = self._split(x_column, threshold)
 
         if len(left_idxs) == 0 or len(right_idxs) == 0:  # check if either are empty
             return 0
 
         # calculate weighted entropy of children
-        n = len(y)
+        n = len(y_data)
         n_left, n_right = len(left_idxs), len(right_idxs)
-        entropy_left, entropy_right = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        entropy_left, entropy_right = self._entropy(y_data[left_idxs]), self._entropy(y_data[right_idxs])
         child_entropy = (n_left/n) * entropy_left + (n_right/n) * entropy_right
 
         # calculate information gain
         information_gain = parent_entropy - child_entropy
         return information_gain
 
-    def _split(self, X_column, split_threshold) -> tuple[np.ndarray, np.ndarray]:
+    def _split(self, x_column: np.ndarray, split_threshold: float) -> tuple[np.ndarray, np.ndarray]:
         """Split the dataset into two groups based on the given split_threshold.
 
         Preconditions:
             - X.column.size > 0
         """
-        left_idxs = np.argwhere(X_column <= split_threshold).flatten()
-        right_idxs = np.argwhere(X_column > split_threshold).flatten()
+        left_idxs = np.argwhere(x_column <= split_threshold).flatten()
+        right_idxs = np.argwhere(x_column > split_threshold).flatten()
         return left_idxs, right_idxs
 
-    def _entropy(self, y) -> float:
+    def _entropy(self, y_data: np.ndarray) -> float:
         """Calculate the entropy of a given set of labels.
 
         The entropy is a measure of the impurity or uncertainty in the labels.
         It ranges from 0 (no uncertainty) to log2(n) (maximum uncertainty).
         """
-        hist = np.bincount(y)
-        prob = hist / len(y)
+        hist = np.bincount(y_data)
+        prob = hist / len(y_data)
         return -np.sum(prob * np.log2(prob + 1e-9))
 
-    def _most_common_label(self, y) -> Optional[int]:
+    def _most_common_label(self, y_data: np.ndarray) -> Optional[int]:
         """Find the most common label in the dataset."""
-        if len(y) == 0:
+        if len(y_data) == 0:
             return None
-        counter = Counter(y)
+        counter = Counter(y_data)
         return counter.most_common(1)[0][0]
 
-    def predict(self, X) -> np.array:
+    def predict(self, x_data: np.ndarray) -> np.ndarray:
         """Predict class labels for a given dataset using the trained decision tree.
 
         Preconditions:
             - self.root is not None
         """
-        return np.array([self._traverse_tree(x, self.root) for x in X])
+        return np.array([self._traverse_tree(x, self.root) for x in x_data])
 
-    def _traverse_tree(self, x, node) -> int:
+    def _traverse_tree(self, x: np.ndarray, node: Node) -> int:
         """Recursively traverse the decision tree to classify a single sample.
 
         If the current node is a leaf, return its stored class label. Otherwise, continue
@@ -205,7 +210,7 @@ class DecisionTree:
 
 
 def recommend_songs(dtree: DecisionTree, user_song: str, features: list[str],
-                    dataset: pd.DataFrame, top_n=5) -> Optional[list[tuple]]:
+                    dataset: pd.DataFrame) -> Optional[list[tuple[str, str, float]]]:
     """Recommend a similar song to the given user_song using the decision tree dtree and cosine similarity.
 
     Preconditions:
@@ -214,7 +219,6 @@ def recommend_songs(dtree: DecisionTree, user_song: str, features: list[str],
         - features is a list of feature names that exist in the dataset
         - dataset is a pandas DataFrame containing at least the column names: 'name', 'artists'
         and the specified features.
-        - top_n > 0 (default value is returning the top 5 recommended songs).
     """
 
     # find user_song features in the dataset
@@ -225,9 +229,9 @@ def recommend_songs(dtree: DecisionTree, user_song: str, features: list[str],
     leaf_node_prediction = dtree.predict(user_song_features_df.to_numpy())[0]
 
     # using LabelEncoder to map the leaf node back to the song name
-    le = LabelEncoder()
-    le.fit(dataset['name'])  # fit the encoder on the dataset's song names
-    predicted_song_name = le.inverse_transform([leaf_node_prediction])[0]
+    encoder = LabelEncoder()
+    encoder.fit(dataset['name'])  # fit the encoder on the dataset's song names
+    predicted_song_name = encoder.inverse_transform([leaf_node_prediction])[0]
 
     # extract songs that belong to the predicted leaf node
     leaf_node_songs = dataset[dataset['name'] == predicted_song_name]
@@ -243,7 +247,7 @@ def recommend_songs(dtree: DecisionTree, user_song: str, features: list[str],
     similarities = []
     user_song_vector = np.array(list(user_song_features.values())).reshape(1, -1)
 
-    for index, row in leaf_node_songs.iterrows():
+    for _, row in leaf_node_songs.iterrows():
         song_features = row.to_dict()
         song_features_values = {k: v for k, v in song_features.items() if k in user_song_features}
         song_vector = np.array(list(song_features_values.values())).reshape(1, -1)
@@ -259,15 +263,16 @@ def recommend_songs(dtree: DecisionTree, user_song: str, features: list[str],
 
     # remove duplicates
     seen = set()
-    recommended_songs = []
-    for song, artists, similarity in similarities:
-        if song not in seen:
-            recommended_songs.append((song, artists, similarity))
-            seen.add(song)
+    rec_songs = []
+    for song_name, artists_names, similarity in similarities:
+        if song_name not in seen:
+            rec_songs.append((song_name, artists_names, similarity))
+            seen.add(song_name)
 
-    return recommended_songs
+    return rec_songs
 
-def calculate_cosine_similarity(user_song_vector, song_vector) -> float:
+
+def calculate_cosine_similarity(user_song_vector: np.ndarray, song_vector: np.ndarray) -> float:
     """Calculates the cosine similarity between the feature vectors of the user's song and a song from the dataset.
     If the shapes of the feature vectors for the user song and dataset song do not match, it returns -1.
     """
@@ -297,13 +302,14 @@ def get_song_features(user_song: str, features: list[str], dataset: pd.DataFrame
     return user_song_features
 
 
-def visualize_custom_tree(node, feature_names, class_names, dot_data=None, parent=None, depth=0):
+def visualize_custom_tree(node: Node, feature_names: list[str], classnames: list[str], dot_data: Optional[str] = None,
+                          parent: Optional[str] = None, depth: Optional[int] = 0) -> Optional[Source]:
     """Recursively traverse the tree and create Graphviz dot representation."""
     if dot_data is None:
         dot_data = "digraph Tree {\n"  # indicates start of the tree representation
 
     if node.is_leaf_node():  # if it is a leaf node, label with class name
-        class_name = class_names[node.value]
+        class_name = classnames[node.value]
         class_name = class_name.replace('"', '\\"')  # get rid of quotes in class name
         dot_data += f'    node{depth} [label="{class_name}"];\n'
         if parent is not None:  # if the node is not the root, add edge between parent node to current leaf node
@@ -315,8 +321,9 @@ def visualize_custom_tree(node, feature_names, class_names, dot_data=None, paren
         if parent is not None:
             dot_data += f'    node{parent} -> node{depth};\n'
 
-        dot_data = visualize_custom_tree(node.left, feature_names, class_names, dot_data, parent=depth, depth=depth + 1)
-        dot_data = visualize_custom_tree(node.right, feature_names, class_names, dot_data, parent=depth, depth=depth + 1)
+        dot_data = visualize_custom_tree(node.left, feature_names, classnames, dot_data, parent=depth, depth=depth + 1)
+        dot_data = visualize_custom_tree(node.right, feature_names, classnames,
+                                         dot_data, parent=depth, depth=depth + 1)
 
     if depth == 0:  # once all the nodes and edges are processed
         dot_data += "}\n"
@@ -325,9 +332,9 @@ def visualize_custom_tree(node, feature_names, class_names, dot_data=None, paren
     return dot_data  # return dot data when the function is not at root level (i.e. depth > 0)
 
 
-def plot_tree(dtree, feature_names, class_names, filename='tree'):
+def plot_tree(dtree: DecisionTree, feature_names: list[str], classnames: list[str], filename: str = 'tree') -> None:
     """Generate the tree visualization and save to a file."""
-    graph = visualize_custom_tree(dtree.root, feature_names, class_names)
+    graph = visualize_custom_tree(dtree.root, feature_names, classnames)
     print(graph.source)
     graph.render(filename, format='png')
     print(f"Tree visualization saved to {filename}.png")
@@ -349,7 +356,9 @@ if __name__ == "__main__":
     LIMIT = 5000  # limit the dataset size to LIMIT rows to reduce running time during testing
 
     # DATA WRANGLING
-    df = pd.read_csv('path-to-file/songs_with_attributes_and_lyrics.csv')  # TODO: replace path-to-file
+    # TODO: replace path-to-file
+    df = pd.read_csv('/Users/jemimasilaen/.cache/kagglehub/datasets/bwandowando/spotify'
+                     '-songs-with-attributes-and-lyrics/versions/19/songs_with_attributes_and_lyrics.csv')
     df = df.dropna(subset=FEATURES)
     df = df.head(LIMIT)
 
@@ -383,9 +392,8 @@ if __name__ == "__main__":
     clf.fit(X_train, y_train)
 
     # (Optional) visualize tree
-    plot_tree(dtree=clf, feature_names=FEATURES, class_names=class_names, filename="song_recommendation_tree")
+    plot_tree(dtree=clf, feature_names=FEATURES, classnames=class_names, filename="song_recommendation_tree")
 
-    # get song recommendations
     # get song recommendations
     recommended_songs = recommend_songs(dtree=clf, user_song=SONG, features=FEATURES, dataset=df)
 
